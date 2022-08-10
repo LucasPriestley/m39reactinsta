e95e5153bb8a19be109d92eb10c0e34ea8a60a20
Lucas <104462643+LucasPriestley@users.noreply.github.com>
2022-08-10 14:10:51 +0100

    updated repo

--- src/components/signupOrLogin.js
+++ src/components/signupOrLogin.js
@@ -3,14 +3,13 @@ import { signUp } from "../utils";
 
 const SignupOrLogin = ({setter}) => {
     const [username, setUsername] = useState();
-    const [firstname, setFirstname] = useState();
-    const [lastname, setLastname] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
     const submitHandler = async (event) => {
         event.preventDefault();
-        await signUp(username, firstname, lastname, email, password, setter );
+        await signUp(username, email, password, setter );
+        console.log(username)
     }
 
 return (
@@ -19,14 +18,6 @@ return (
             <input onChange={(event) => setUsername(event.target.value)} />
         </label>
         <br></br>
-        <label>First name:
-            <input onChange={(event) => setFirstname(event.target.value)} />
-        </label>
-        <br></br>  
-        <label>Last name:
-            <input onChange={(event) => setLastname(event.target.value)} />
-        </label>
-        <br></br>  
         <label>Email:
             <input type="email" onChange={(event) => setEmail(event.target.value)} />
         </label>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,21 @@
-export const signUp = async (username, firstname, lastname, email, password, setter ) => {
+export const signUp = async (username, email, password, setter ) => {
+  
   try {
-    const response = await fetch("http://localhost:5000/users",{
+    const response = await fetch("http://localhost:5000/user",{
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
             "username": username,
-            "firstName": firstname,
-            "lastName": lastname,
             "email": email,
             "password": password
         })
     });
     const data = await response.json();
-    console.log(data.saveUser.userName);
-    setter(data.saveUser.userName);
+    console.log(data)
+    setter(data.newUser.username);
 
   } catch (error) {
     console.log(error);
   }
 };
+
